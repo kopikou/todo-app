e64dc99374488214e784022aaba0f3fa79029860
Kopikou <copilowa.nadezhda2015@yandex.ru>
2025-09-16 18:01:34 +0800

    change delete

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ def complete_todo(todo_id):
 
 @app.route('/delete/<int:todo_id>')
 def delete_todo(todo_id):
-    global todos
     todos = [todo for todo in todos if todo['id'] != todo_id]
     return redirect('/')
 
